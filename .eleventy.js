862ed40b90b8807ddd07ed432bfd144f32497684
Tô Quốc Anh <bagu.kr.vn@gmail.com>
2025-07-19 12:32:40 +0700

    Resolve all merge conflicts

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 module.exports = function (eleventyConfig) {
-  // Copy thư mục public vào _site
   eleventyConfig.addPassthroughCopy("public");
-
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site",
-    },
+      output: "_site"
+    }
   };
 };
